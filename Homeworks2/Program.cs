061b3a8034e36ff1ffcc6ad23459d7cca3153122
apelsina8 <arhishow@gmail.com>
2022-12-02 01:00:29 +0300

    test

---
+++ Homeworks2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Input namber 1");  
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input namber 2");  
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int a = num1;
+int b = num2;
+
+int max = a;
+int min = b;
+    
+if ( a > max) max = a;
+{  
+     Console.WriteLine($"{num1} > {num2}");  
+} 
